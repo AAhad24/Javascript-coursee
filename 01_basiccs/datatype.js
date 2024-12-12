bb5c99b658720fd5415ab62f0677e31f7ba94a93
AAhad24 <abdul.bscs4447@iiu.edu.pk>
2024-12-12 17:26:08 +0000

    datatypes and variables

---
+++ 01_basiccs/datatype.js
@@ -0,0 +1,3 @@
+"use strict";// treat all js code as newwer version
+
+// alert(3 +3 )//  we are using node js ,not browser
---
+++ 01_basiccs/variable.js
@@ -0,0 +1,12 @@
+const accountID = 144435
+let   accountEmail= "ahad@gmail"
+var   accountPassword= "12345"
+accountcity = "isb" // 
+let accountState;
+
+
+console.log(accountID)
+accountEmail= "asaad@gmail"
+accountPassword= "7866"
+
+console.table([accountID,accountEmail,  accountPassword, accountState])
